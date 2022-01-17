dcbc0e6e6a35187329b1b7139a917ffe0665ced8
JosherTmr <joshertmr@hotmail.com>
2022-01-17 09:50:46 -0500

    Test breackpoint 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -148,6 +148,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  suma=0;
+  for (var i=0; i<usuario['posts'].length;i++){
+    suma += usuario['posts'][i]['likes'];
+  }
+  return(suma)
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -160,7 +165,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto['calcularPrecioDescuento']=function(){
+    return(this.precio-=this.precio*this.porcentajeDeDescuento)
+  }
+  return(producto)
 }
 
 // No modificar nada debajo de esta línea
